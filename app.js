03a6ad2d4462b77f526d2f3f8c852fa00a6d15d6
Stephen Warton <stephen.warton@gmail.com>
2017-08-05 16:32:20 -0600

    testing data sent back

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const io = require('socket.io-client');
 
 var socket = io.connect('https://constellation.herokuapp.com/');
+
 socket.on('users', function(data) {
   console.log(data.concurrentUsers);
+});
+
+socket.on('action', function(data) {
+  console.log(data.data);
 });

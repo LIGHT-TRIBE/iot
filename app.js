6ad88e6c4727ff8c1f9cef60f4aa180fa5c94d1d
Stephen Warton <stephen.warton@gmail.com>
2017-08-05 16:10:50 -0600

    test

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 //initial commit
+const io = require('socket.io-client');
+
+var socket = io.connect('https://constellation.herokuapp.com/');
+socket.on('users', (data)=>{
+  console.log(data.concurrentUsers);
+});

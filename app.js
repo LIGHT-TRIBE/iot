3ae5ab5efc29c149e09dd7d4f00fb40def4fb207
Stephen Warton <stephen.warton@gmail.com>
2017-08-05 20:29:34 -0600

    console logging correct info

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const io = require('socket.io-client');
 
 var socket = io.connect('https://constellation.herokuapp.com/');
 
+const initState=()=>{socket.on('init', function(data){
+  console.log(data)
+})}
+
+initState()
+
 socket.on('pi', function(data) {
-  console.log(data.data);
+  console.log(data);
 });
 
 socket.on('update', function(data) {
+  console.log(data);
+});
+
+socket.on('users', function(data) {
   console.log(data);
 });

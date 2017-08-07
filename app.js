c234ff3c96a41d93750d55279a0ad0ac40cea8e5
pi <willmeierart@gmail.com>
2017-08-07 00:42:12 +0000

    update

--- app.js
+++ app.js
@@ -6,21 +6,22 @@ var matrix = new LedMatrix(rows=32,chainedDisplays=2)
 var socket = io.connect('https://constellation.herokuapp.com/');
 
 var initState=()=>{socket.on('init', function(data){
-  console.log(data)
+ // console.log(data)
 })}
 
 
 initState()
 
 socket.on('init', function(data) {
-  for (var i = 0; i < data.length; i++){
+console.log(data)
+  for (var i = 0; i < data.data.length; i++){
     matrix.setPixel(data.data[0],data.data[1],data.data[2],data.data[3],data.data[4])
   }
 })
 socket.on('users', function(data) {
-  console.log(data);
+  //console.log(data);
 })
 socket.on('update', function(data) {
-  console.log(data);
+  //console.log(data);
 	matrix.setPixel(data.data[0],data.data[1],data.data[2],data.data[3],data.data[4])
 })

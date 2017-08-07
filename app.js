08ad1f7e3fe053e3ac1ead79b175ecd332a027ed
WILL MEIER <willmeierart@gmail.com>
2017-08-06 18:44:36 -0600

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var io = require('socket.io-client');
 var LedMatrix = require("node-rpi-rgb-led-matrix");
 
-var matrix = new LedMatrix(rows=32,chainedDisplays=2)
+var matrix = new LedMatrix(32,2)
 
 var socket = io.connect('https://constellation.herokuapp.com/');
 
@@ -13,15 +13,15 @@ var initState=()=>{socket.on('init', function(data){
 initState()
 
 socket.on('init', function(data) {
-console.log(data)
+console.log('socket init ', data)
   for (var i = 0; i < data.data.length; i++){
     matrix.setPixel(data.data[0],data.data[1],data.data[2],data.data[3],data.data[4])
   }
 })
 socket.on('users', function(data) {
-  //console.log(data);
+  console.log('socket users ', data);
 })
 socket.on('update', function(data) {
-  //console.log(data);
+  console.log('socket update ', data);
 	matrix.setPixel(data.data[0],data.data[1],data.data[2],data.data[3],data.data[4])
 })
